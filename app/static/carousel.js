9d24d81e69091f970ba379264ba30b0dfecdf320
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-24 22:56:17 +0300

    refactor: Enhance styles for better readability and consistency
    - Remove redundant carousel.js file
    - Update styles in style.css for improved typography and spacing

--- app/static/carousel.js
+++
@@ -1,6 +0,0 @@
-let myCarousel = document.querySelector('#carouselExampleCaptions');
-let carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2000, // Время между слайдами (в миллисекундах)
-    wrap: true, // Зацикливание слайдов
-    pause: 'hover' // Пауза при наведении
-});
